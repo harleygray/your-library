8868b58ae58c7eac8cf9beabd6d900daddd768f5
Harley <harley.gray.96@gmail.com>
2023-07-12 15:45:36 +1000

    Adding context to search (name, tags, etc)

--- pages/library.py
+++ pages/library.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-
 WEAVIATE_URL = os.getenv("WEAVIATE_URL")
 WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
@@ -14,18 +13,11 @@ st.set_page_config(
     page_icon='📚', 
     page_title="your library",
     initial_sidebar_state="collapsed")
-st.header("current library contents")
-show_library = False
-
+st.header("library contents")
+st.write("search the contents of your library via semantic search")
 
 # url to embed into personal library
 # https://towardsdatascience.com/a-multi-page-interactive-dashboard-with-streamlit-and-plotly-c3182443871a
-
-
-
-# Drop down streamlit component to select available classes
-# st.selectbox(
-#     label="select class",
 
 client = weaviate.Client(
     url=WEAVIATE_URL,
@@ -35,19 +27,40 @@ client = weaviate.Client(
     }
 )
 
-schema = client.schema.get()
-# Get class list from schema
-class_list = [class_dict['class'] for class_dict in schema['classes']]
-st.write("For now only the ABCNewsArticle class is available")
-selected_class = st.selectbox('Select a class', ["ABCNewsArticle"])
 
-# Now you can use selected_class in your application logic
-st.write(f"You selected {selected_class}")
+# Set initial state
+keys = [
+    'selected_tag', 'selected_class', 'concepts_input', 'result_limit', 'results',
+    'results_table'
+]
+default_values = [
+    None, None, None, None, None, pd.DataFrame(columns=['group_header', 'text_list'])]
+for key, default_value in zip(keys, default_values):
+    if key not in st.session_state:
+        st.session_state[key] = default_value
+
+
+
+# Get class list from schema
+schema = client.schema.get()
+class_list = [class_dict['class'] for class_dict in schema['classes']]
+
+selected_class, selected_tag = st.columns(2)
+
+
+with selected_class:
+    st.session_state.selected_class = st.selectbox('select a class (only ABCNewsArticle is available currently)', ["ABCNewsArticle"])
+
+
+
+# Create a dropdown selector for the class
 
 # Query Weaviate to get the data objects in the selected class
 objects = (
     client.query
-    .get(selected_class, ["tags", "text", "article_name", "date", "url", "upload_note"])
+    .get(
+        st.session_state.selected_class, 
+        ["tags", "text", "article_name", "date", "url", "upload_note"])
     .with_where({
         "path": ["category"],
         "operator": "Equal",
@@ -56,11 +69,10 @@ objects = (
     .do()
 )
 
-st.write(objects)
 
 # Extract the unique tags values
 tags = set()
-for obj in objects['data']['Get'][selected_class]:
+for obj in objects['data']['Get'][st.session_state.selected_class]:
     if 'tags' in obj:
         # Split the tags string into individual tags
         individual_tags = obj['tags'].split(', ')
@@ -68,23 +80,30 @@ for obj in objects['data']['Get'][selected_class]:
         tags.update(individual_tags)
 
 # Create a dropdown selector for the tags
-selected_tag = st.selectbox('Select a tag', list(tags))
+with selected_tag:
+    st.session_state.selected_tag = st.selectbox('Select a tag', list(tags))
+
+
 
 def reset_results():
     st.session_state['results'] = []
+    st.session_state['results_table'] = pd.DataFrame(columns=['group_header', 'text_list'])
 
-# show objects of the matching tag and class close to a similarity search
-st.write("show objects of the matching tag and class close to a similarity search")
-# user input for concepts, comma separated
-concepts_input = st.text_input("enter search query", "cost of living")
-concepts = concepts_input.split(", ")
+concepts_input, result_limit = st.columns(2)
+
+# User input for concepts, comma separated
+with concepts_input:
+    st.session_state.concepts_input = st.text_input("enter search query to use for similarity search", "cost of living").split(", ")
+with result_limit:
+    st.session_state.result_limit = st.slider("result limit", 1, 10, 4)
+
 max_distance = 0.3
 nearText = {
-    "concepts": concepts,
+    "concepts": st.session_state.concepts_input,
     "distance": max_distance}
 
 search_button = st.empty()
-result_limit = st.slider("result limit", 1, 10, 4)
+
 results_table = st.empty()
 
 with search_button:
@@ -94,7 +113,7 @@ with search_button:
             client.query  # start a new query
             .get("ABCNewsArticle", ["text", "article_name", "upload_note", "tags"])  
             .with_near_text(nearText)  
-            .with_limit(result_limit) 
+            .with_limit(st.session_state.result_limit) 
             .with_where({
                 "path": ["category"],
                 "operator": "Equal",
@@ -103,9 +122,9 @@ with search_button:
             .do() 
         )
         # show the objects with the selected tag
-        for obj in response['data']['Get'][selected_class]:
+        for obj in response['data']['Get'][st.session_state.selected_class]:
             if 'tags' in obj:
-                if selected_tag in obj['tags']:
+                if st.session_state.selected_tag in obj['tags']:
                     st.session_state['results'].append(obj)
 
         df = pd.DataFrame(st.session_state['results'])
@@ -118,33 +137,29 @@ with search_button:
         # group by tags, input notes, and article names, and aggregate the text
         grouped_df = df.groupby(['tags', 'upload_note', 'article_name'])['text'].agg(aggregate_texts).reset_index()
         
-        
+        results_df = st.session_state.results_table
         
         for _, group in grouped_df.groupby(['tags', 'upload_note', 'article_name']):
             # Create a markdown string for the group header
-            group_header = f"**Tags:** {group['tags'].iloc[0]}  \n**Upload Note:** {group['upload_note'].iloc[0]}  \n**Article Name:** {group['article_name'].iloc[0]}"
-            st.write(group_header)
-            
+            group_header = f"`Article Name: ` {group['article_name'].iloc[0]}   \n`Tags: ` {group['tags'].iloc[0]}  \n`Upload Note: ` {group['upload_note'].iloc[0]}"
             # Create a markdown string for the ordered list of texts
             text_list = "\n".join(f"{text}" for text in group['text'])
-            st.markdown(text_list)
+            # Append the group header and text list to the results DataFrame
+            results_df = pd.concat([results_df, pd.DataFrame([{'group_header': group_header, 'text_list': text_list}])], ignore_index=True)
 
 
+        
+        # Save the results DataFrame to the Streamlit session state
+        st.session_state.results_table = results_df
 
-st.button(
-    label="fetch from library",
-    on_click=helper.fetch_from_weaviate(
-        helper.format_graphql_query("test")))
+for i in range(len(st.session_state.results_table)):
+    st.markdown(st.session_state.results_table['group_header'][i])
+    st.markdown(st.session_state.results_table['text_list'][i])
 
-# unsure here of how i get the response into a table
 
 
 st.button(
     label="generate summaries (not working yet)" 
 )
-
-#formatted_graphql_response = helper.format_for_viewing(graphql_response)
-# st.table(graphql_response)
-
 
 
