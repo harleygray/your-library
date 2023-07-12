b46a03244dca1d428b9dd56f6bef97dd642baa5b
Harley <harley.gray.96@gmail.com>
2023-07-12 13:13:34 +1000

    add search for library

--- pages/library.py
+++ pages/library.py
@@ -10,7 +10,10 @@ load_dotenv()
 WEAVIATE_URL = os.getenv("WEAVIATE_URL")
 WEAVIATE_API_KEY = os.getenv("WEAVIATE_API_KEY")
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
-
+st.set_page_config(
+    page_icon='📚', 
+    page_title="your library",
+    initial_sidebar_state="collapsed")
 st.header("current library contents")
 show_library = False
 
@@ -67,12 +70,64 @@ for obj in objects['data']['Get'][selected_class]:
 # Create a dropdown selector for the tags
 selected_tag = st.selectbox('Select a tag', list(tags))
 
+def reset_results():
+    st.session_state['results'] = []
 
-# show the objects with the selected tag
-for obj in objects['data']['Get'][selected_class]:
-    if 'tags' in obj:
-        if selected_tag in obj['tags']:
-            st.write(obj)
+# show objects of the matching tag and class close to a similarity search
+st.write("show objects of the matching tag and class close to a similarity search")
+# user input for concepts, comma separated
+concepts_input = st.text_input("enter search query", "cost of living")
+concepts = concepts_input.split(", ")
+max_distance = 0.3
+nearText = {
+    "concepts": concepts,
+    "distance": max_distance}
+
+search_button = st.empty()
+result_limit = st.slider("result limit", 1, 10, 4)
+results_table = st.empty()
+
+with search_button:
+    if st.button("search"):
+        reset_results()
+        response = (
+            client.query  # start a new query
+            .get("ABCNewsArticle", ["text", "article_name", "upload_note", "tags"])  
+            .with_near_text(nearText)  
+            .with_limit(result_limit) 
+            .with_where({
+                "path": ["category"],
+                "operator": "Equal",
+                "valueString": "NarrativeText"
+            })
+            .do() 
+        )
+        # show the objects with the selected tag
+        for obj in response['data']['Get'][selected_class]:
+            if 'tags' in obj:
+                if selected_tag in obj['tags']:
+                    st.session_state['results'].append(obj)
+
+        df = pd.DataFrame(st.session_state['results'])
+        # add a relevance score column
+        df['relevance_score'] = df.index + 1
+        # define a custom aggregation function
+        def aggregate_texts(series):
+            return '\n'.join(f"{i+1}. {text}" for i, text in enumerate(series))
+
+        # group by tags, input notes, and article names, and aggregate the text
+        grouped_df = df.groupby(['tags', 'upload_note', 'article_name'])['text'].agg(aggregate_texts).reset_index()
+        
+        
+        
+        for _, group in grouped_df.groupby(['tags', 'upload_note', 'article_name']):
+            # Create a markdown string for the group header
+            group_header = f"**Tags:** {group['tags'].iloc[0]}  \n**Upload Note:** {group['upload_note'].iloc[0]}  \n**Article Name:** {group['article_name'].iloc[0]}"
+            st.write(group_header)
+            
+            # Create a markdown string for the ordered list of texts
+            text_list = "\n".join(f"{text}" for text in group['text'])
+            st.markdown(text_list)
 
 
 
@@ -83,12 +138,9 @@ st.button(
 
 # unsure here of how i get the response into a table
 
-st.table(pd.read_csv("data/parsed_pdf.csv"))
-graphql_response = ""
 
 st.button(
-    label="generate summaries",
-    on_click=helper.summarise_embeds(graphql_response) # changes graphql_response
+    label="generate summaries (not working yet)" 
 )
 
 #formatted_graphql_response = helper.format_for_viewing(graphql_response)
