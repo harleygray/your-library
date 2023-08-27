63834eb6b1b8e5e5f7b37370745ae2a521e4343b
Harley <harley.gray.96@gmail.com>
2023-08-28 05:15:26 +1000

    show voice to parliament home page

--- 0_🌟_your_library.py
+++ 0_🌟_your_library.py
@@ -21,7 +21,7 @@ st.write("this website is a work in progress.  here's the functionality availabl
 
 # link to library_search.py
 st.header("search your library")   
-st.markdown("this page is where you find information relevant to what you're looking for. let's say you have 500 news articles stored, but you only wanted to find the ones about cost of living. you can search for 'cost of living' and the most relevant sections of these articles will be returned.")
+st.markdown("this page allows you to search information relevant to the Yes campaign for the Voice to Parliament referendum")
 
 iframe = """
 <iframe
@@ -34,40 +34,41 @@ iframe = """
 components.html(iframe, height=450)
 
 
-# link to unstructured_document_upload.py
-st.header("upload a document")   
-st.markdown("this page allows you to upload any document (works best for text or pdf). any text in the document is organised, cleaned, then added to a [vector database](https://weaviate.io/). this allows you to query the information in that text - a personal search engine.")
-
-iframe = """
-<iframe
-  src="https://your-library.streamlit.app/document_upload/?embed=true"
-  height="450"
-  style="width:100%;border:none;"
-></iframe>
-"""
-
-components.html(iframe, height=450)
-
-
-
-
-
-# link to abc_article.py
-st.header("add abc news article")   
-st.markdown("""
-    here you can link to an australian broadcasting corporation (abc) article and have it added to your library. the abc is a state-funded news organisation and an impactful source of information. 
-
-    this page will eventually be an automated pipeline; article uploaded > added to your library. for now, you can add an article by pasting the url into the text box below.
-    """
-)
-
-
-iframe = """
-<iframe
-  src="https://your-library.streamlit.app/abc_article/?embed=true"
-  height="450"
-  style="width:100%;border:none;"
-></iframe>
-"""
-
-components.html(iframe, height=450)
+## link to unstructured_document_upload.py
+#st.header("upload a document")   
+#st.markdown("this page allows you to upload any document (works best for text or pdf). any text in the document is organised, cleaned, then added to a [vector database](https://weaviate.io/). this allows you to query the information in that text - a personal search engine.")
+#
+#iframe = """
+#<iframe
+#  src="https://your-library.streamlit.app/document_upload/?embed=true"
+#  height="450"
+#  style="width:100%;border:none;"
+#></iframe>
+#"""
+#
+#components.html(iframe, height=450)
+#
+#
+#
+#
+#
+## link to abc_article.py
+#st.header("add abc news article")   
+#st.markdown("""
+#    here you can link to an australian broadcasting corporation (abc) article and have it added to your library. the abc is a state-funded news organisation and an impactful source of information. 
+#
+#    this page will eventually be an automated pipeline; article uploaded > added to your library. for now, you can add an article by pasting the url into the text box below.
+#    """
+#)
+#
+#
+#iframe = """
+#<iframe
+#  src="https://your-library.streamlit.app/abc_article/?embed=true"
+#  height="450"
+#  style="width:100%;border:none;"
+#></iframe>
+#"""
+#
+#components.html(iframe, height=450)
+#
