bc23a60ac2a8b641116dc5ddd43984893c8628d9
Harley <harley.gray.96@gmail.com>
2023-10-30 15:12:51 +1000

    misc updates

--- 0_🌟_your_library.py
+++ 0_🌟_your_library.py
@@ -6,6 +6,7 @@ import pandas as pd
 from dotenv import load_dotenv
 import plotly.graph_objs as go
 import base64
+import re
 
 def create_individual_politicians_dict(members):
     individual_politicians = {}
@@ -65,6 +66,19 @@ def format_division_data(division_data):
 
     return individual_politicians_df
 
+def plot_parliament(individual_votes, active_division):
+    
+
+
+    fig = go.Figure(data=go.Scatter(
+        x=[1, 2, 3, 4],
+        y=[10, 11, 12, 13],
+        mode='markers',
+        marker=dict(size=[40, 60, 80, 100],
+                    color=[0, 1, 2, 3])
+    ))
+    return fig
+
 # def plot_parliament(individual_votes, active_division):
 #     house = active_division["house"]
     
@@ -110,6 +124,17 @@ def format_division_data(division_data):
 #     st.pyplot(fig)  # Display the plot using Streamlit
 
 @st.cache_data()
+def load_vote_template(filename):
+    # Check if the file with all members exists
+    if os.path.exists(filename):
+        # Load data from the file if it exists
+        with open(filename, 'r') as f:
+            return json.load(f)
+    else:
+        st.write("No local member data found.")
+        return None
+
+@st.cache_data()
 def load_members_from_files(filename):
     # Check if the file with all members exists
     if os.path.exists(filename):
@@ -129,7 +154,7 @@ def load_divisions_from_files():
     # Check if the file with all divisions exists
     if os.path.exists(filename):
         # Load data from the file if it exists
-        with open(filename, 'r') as f:
+        with open(filename, 'r', encoding='utf-8') as f:
             return json.load(f)
     else:
         st.write("No local division data found.")
@@ -299,6 +324,62 @@ def generate_unique_id(member):
     party = member.get('party', 'Unknown')
     return f"{first_name}_{last_name}_{party}"
 
+def categorise_divisions(division_names):
+    categories = {
+        'Matters of Urgency': [],
+        'Business': [],
+        'Documents': [],
+        'Committees': [],
+        'Motions': [],
+        'Bills': {}
+    }
+
+    for division in division_names:
+        categorized = False
+        for category in categories.keys():
+            if division.startswith(category):
+                clean_name = division[len(category) + 3:]  # +3 to remove the " - " as well
+                categories[category].append(clean_name)
+                categorized = True
+                break
+        if not categorized:
+            bill_split = re.split(r'(\d{4}(?:-\d{4})?) - ', division)
+            if len(bill_split) >= 3:
+                bill_title = bill_split[0] + bill_split[1]
+                bill_stage = bill_split[2]
+                categories['Bills'].setdefault(bill_title, []).append(bill_stage)
+    return categories
+
+@st.cache_data()
+def return_division(full_name, divisions):
+    division = next(
+        (division for division in divisions.values() if division['name'] == full_name), None)
+    return division
+
+@st.cache_data()
+def background_image(content1, content2):
+    with open("static/img/Parliament-House-Australia-Thennicke.jpg", "rb") as image_file:
+        image_data = base64.b64encode(image_file.read()).decode()
+    
+    background_image_html = f'url(data:image/png;base64,{image_data})'
+    st.markdown(
+        f'<h1 style="text-align:center;background-image: {background_image_html};background-size: 100% 100%;'
+        f'font-size:60px;border-radius:2%;padding-top:35%;padding-bottom:10%;">'  
+        f'<span style="background-color: rgb(69,69,92,0.5); color:white;font-size:40px;">{content1}</span><br>'
+        f'<span style="background-color: rgb(69,69,92,0.6); color:white;font-size:19px;">{content2}</span></h1>',
+        unsafe_allow_html=True
+    )
+
+
+
+
+
+    # st.markdown(f'<h1 style="text-align:center;background-image: linear-gradient(to right,{color1}, {color2});font-size:60px;border-radius:2%;">'
+    #             f'<span style="color:{color3};">{content1}</span><br>'
+    #             f'<span style="color:white;font-size:17px;">{content2}</span></h1>', 
+    #             unsafe_allow_html=True)
+
+
 def main():
     # Load environment variables
     load_dotenv()
@@ -309,49 +390,71 @@ def main():
         page_icon='🗳️', 
         page_title="parliamentary divisions", 
         initial_sidebar_state="collapsed",
-        layout="wide")
+        layout="centered")
     
     # Set initial state
-    keys = ['divisions', 'senate', 'representatives']
-    default_values = [None, None, None]
+    keys = ['divisions', 'senate', 'representatives', 'selected_division']
+    default_values = [None, None, None, None]
 
     for key, default_value in zip(keys, default_values):
         if key not in st.session_state:
             st.session_state[key] = default_value
 
-    # Load assets
-    with open("static/img/Parliament-House-Australia-Thennicke.jpg", "rb") as image_file:
-        image_data = base64.b64encode(image_file.read()).decode()
-    photo_html = f'<img src="data:image/png;base64,{image_data}" width="450" alt="Parliament House" title="Parliament House"></a>'
+    
     
     st.session_state['representatives'] = load_members_from_files('./data/parliament/house.json')
     st.session_state['senate'] = load_members_from_files('./data/parliament/senate.json')
     st.session_state['divisions'] = load_divisions_from_files()
 
     # Header
-    st.markdown("### a window into parliament")
-    st.markdown('''a tool to clearly display the proceedings of government''')
-    st.markdown(photo_html, unsafe_allow_html=True)
-    st.divider()
+    background_image("window into parliament", "a tool to clearly display the proceedings of government")
+    st.write()
+
+    #st.markdown(photo_html, unsafe_allow_html=True)
+    
     
     if st.session_state['divisions'] is None or st.session_state['senate'] is None or st.session_state['representatives'] is None:
         st.write('data not loaded in yet')
     else:
-        st.write("here you can select a 'division': a vote in either the house of representatives or the senate")
+        st.write("⬇️ here you can select a 'division': a vote in either the house of representatives or the senate")
+        
+        col1, col2 = st.columns([0.3,0.7])
         division_names = [division['name'] for key, division in st.session_state['divisions'].items()]
         
-        # User input for selecting a division
-        selected_division_name = st.selectbox(label="select division", options=division_names)
+        divisions_dict = categorise_divisions(division_names)
+        with col1:
+            selected_division_category =  st.radio(label='pick a type of division', options=list(divisions_dict.keys()))
+        
+        with col2:
+            if selected_division_category == 'Bills':
+        
+                selected_bill_name = st.selectbox(label='pick a bill', options=list(divisions_dict['Bills'].keys()))
+                #st.write(divisions_dict['Bills'][selected_bill_name])
+                if len(divisions_dict['Bills'][selected_bill_name]) == 1:
+                    full_name = selected_bill_name + ' - ' + divisions_dict['Bills'][selected_bill_name][0]
+                else:
+                    selected_bill_reading = st.selectbox(label='which division?', options=list(divisions_dict['Bills'][selected_bill_name]))
+                    full_name = selected_bill_name + ' - ' + selected_bill_reading
+                
+                st.write(full_name)
+                
+            else:
+                
+                selected_division = st.selectbox(label='pick a division', options=list(divisions_dict[selected_division_category]))
+                full_name = selected_division_category + ' - ' + selected_division
+                #st.write(full_name)
+        
+        # Hold selected division in session state
+        st.session_state['selected_division'] = return_division(full_name, st.session_state['divisions'])
+        
+        #st.divider()
+        individual_votes = format_division_data(st.session_state['selected_division'])
 
-        # Format voting data for division
-        selected_division = next(
-            (division for division in st.session_state['divisions'].values() if division['name'] == selected_division_name), None)
-        individual_votes = format_division_data(selected_division)
+
         
 
-        st.markdown(selected_division['summary'])
-        st.divider()
-        st.write("this is a breakdown of the vote. the votes for major parties are highlighted by default, change this by clicking the other tabs")
+        
+        #st.write("votes for major parties are highlighted by default. change this by clicking the other tabs")
 
         # Dictionary to classify parties as major vs minor/independent
         party_dict = {
@@ -371,22 +474,39 @@ def main():
         party_dict['all_members']['senate'] = party_dict['major_parties']['senate'] + party_dict['minor_independents']['senate']
         party_dict['all_members']['representatives'] = party_dict['major_parties']['representatives'] + party_dict['minor_independents']['representatives']
 
+        selected_house = st.session_state['selected_division']['house']
+
         # Generate figures 
-        fig_major = plotly_vote_breakdown(individual_votes, party_dict['major_parties'][selected_division['house']])
-        fig_minor = plotly_vote_breakdown(individual_votes, party_dict['minor_independents'][selected_division['house']])
-        fig_all = plotly_vote_breakdown(individual_votes, party_dict['all_members'][selected_division['house']])
+        fig_major = plotly_vote_breakdown(individual_votes, party_dict['major_parties'][selected_house])
+        fig_minor = plotly_vote_breakdown(individual_votes, party_dict['minor_independents'][selected_house])
+        fig_all = plotly_vote_breakdown(individual_votes, party_dict['all_members'][selected_house])
 
         # Display figures
         major_parties, minor_independents, all_members = st.tabs(['major parties', 'minor parties & independents', 'all members'])
         with major_parties:
+            #st.markdown(
+            #    f'<h1 style="text-align:center;background-image: {background_image_html};background-size: 100% 100%;'
+            #    f'font-size:60px;border-radius:2%;padding-top:35%;padding-bottom:10%;">'  
+            #    f'<span style="background-color: rgb(69,69,92,0.6); color:white;font-size:19px;">{st.session_state['selected_division']['name']}#</span></h1>',
+            #    unsafe_allow_html=True
+
+
+            #)
+            st.subheader(st.session_state['selected_division']['name'])
             st.plotly_chart(fig_major, use_container_width=True)
         with minor_independents:
+            st.subheader(st.session_state['selected_division']['name'])
             st.plotly_chart(fig_minor, use_container_width=True)
         with all_members:
+            st.subheader(st.session_state['selected_division']['name'])
             st.plotly_chart(fig_all, use_container_width=True)
         
         with st.expander("individual member votes"):
             st.dataframe(individual_votes, use_container_width=True, hide_index=True)
+
+        st.markdown(st.session_state['selected_division']['summary'])
+        st.divider()
+            
 
 
 if __name__ == '__main__':
